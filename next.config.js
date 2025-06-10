f842f9122cb46cd046d88eae6cff35a8342844b0
kiensegiau <phanhuukien2001@gmail.com>
2025-06-10 11:13:16 +0700

    Fix build errors

--- next.config.js
+++ next.config.js
@@ -4,6 +4,16 @@ const nextConfig = {
   reactStrictMode: true,
   images: {
     domains: ['lh3.googleusercontent.com', 'i.imgur.com', 'firebasestorage.googleapis.com'],
+  },
+  webpack: (config, { isServer }) => {
+    // Loại trừ các thư viện gây lỗi
+    config.module = {
+      ...config.module,
+      exprContextCritical: false,
+      unknownContextCritical: false
+    };
+    
+    return config;
   }
 }
 
