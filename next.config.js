32f2f3cad1e3773d4de21762c29bcd157eb01c19
kiensegiau <phanhuukien2001@gmail.com>
2025-06-10 11:34:14 +0700

    Fix build issue with node-apiless-youtube-upload

--- next.config.js
+++ next.config.js
@@ -5,6 +5,10 @@ const nextConfig = {
   images: {
     domains: ['lh3.googleusercontent.com', 'i.imgur.com', 'firebasestorage.googleapis.com'],
   },
+  // Exclude problematic modules from the server build
+  experimental: {
+    serverComponentsExternalPackages: ['node-apiless-youtube-upload']
+  },
   webpack: (config, { isServer }) => {
     // Loại trừ các thư viện gây lỗi
     config.module = {
@@ -12,6 +16,12 @@ const nextConfig = {
       exprContextCritical: false,
       unknownContextCritical: false
     };
+    
+    // Only apply this on the server build
+    if (isServer) {
+      // Keep these modules as external and don't try to bundle them
+      config.externals.push('node-apiless-youtube-upload');
+    }
     
     return config;
   }
